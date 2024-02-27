ad6b423b4ac6ac2b4f5ca86c9b9d48ef789ef9a8
RajWarudkar <RajWarudkar>
2024-02-27 18:26:44 +0530

    Assignment

---
+++ build/_next/static/ufX38bJCG2hdm66JxJehL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/bf9d4aa58c9b35aa.css","static/chunks/pages/index-dc8a48c05d588ccf.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
